8da35c7532f16ad4d6b1c97865ccd6d2b4b4c99a
urly3 <u@rl.yyy>
2026-04-25 01:27:06 +0100

    uhhhh

--- build.zig
+++ build.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b; // stub
+    const target = b.standardTargetOptions(.{});
+    const optimise = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "ztftp",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .optimize = optimise,
+            .target = target,
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "run the executable");
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    run_step.dependOn(&run_cmd.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,100 @@
 const std = @import("std");
+const net = std.Io.net;
 
-const op_code = union(enum) {
-    none: u16,
-    rrq: u16,
-    wrq: u16,
-    data: u16,
-    ack: u16,
-    err: u16,
+const OpCode = enum(u16) {
+    none,
+    rrq,
+    wrq,
+    data,
+    ack,
+    err,
 };
 
-const error_code = union(enum) {
-    undefined: u16,
-    file_not_found: u16,
-    access_violation: u16,
-    disk_full: u16,
-    illegal_op: u16,
-    unknown_id: u16,
-    file_exists: u16,
-    no_such_user: u16,
+const ErrorCode = enum(u16) {
+    undefined,
+    file_not_found,
+    access_violation,
+    disk_full,
+    illegal_op,
+    unknown_id,
+    file_exists,
+    no_such_user,
 };
+
+const max_block_size = 512;
+const max_retries = 2;
+var current_block: u32 = 0;
+var bytes_sent: u64 = 0;
+var resends: u16 = 0;
+
+pub fn main(init: std.process.Init) !void {
+    var addr: net.IpAddress = undefined;
+    var filename: []u8 = undefined;
+    const io = init.io;
+
+    {
+        var host_str: []const u8 = undefined;
+        var filename_str: []const u8 = undefined;
+
+        var it = try init.minimal.args.iterateAllocator(init.gpa);
+        defer it.deinit();
+
+        // skip first path arg
+        _ = it.skip();
+
+        var i: usize = 0;
+        while (it.next()) |arg| : (i += 1) {
+            if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+                printHelp();
+                return;
+            }
+
+            if (i == 0) {
+                host_str = arg;
+            }
+
+            if (i == 1) {
+                filename_str = arg;
+            }
+        }
+
+        if (i != 2) {
+            std.debug.print("i = {d}\n", .{i});
+            printHelp();
+            return;
+        }
+
+        addr = try net.IpAddress.parseLiteral(host_str);
+        filename = try init.arena.allocator().alloc(u8, filename_str.len);
+        @memcpy(filename, filename_str);
+    }
+
+    try start(io, addr, filename);
+}
+
+fn start(io: std.Io, addr: net.IpAddress, filename: []const u8) !void {
+    // try to open the file before doing anything
+
+    var buffer: [512]u8 = @splat(0);
+    var stream = try addr.connect(io, .{ .mode = .dgram, .protocol = .udp });
+    var w = stream.writer(io, &buffer).interface;
+    try sendRequest(&w, filename);
+}
+
+fn sendRequest(w: *std.Io.Writer, filename: []const u8) !void {
+    try w.writeInt(u16, 2, .big);
+    _ = try w.write(filename);
+    try w.flush();
+}
+
+fn recieveAck(r: *std.Io.Reader) !bool {
+    _ = r; // autofix
+}
+
+fn sendBlock(w: *std.Io.Writer) !void {
+    _ = w; // autofix
+}
+
+fn printHelp() void {
+    std.debug.print("ztftp ip:port filename", .{});
+}
